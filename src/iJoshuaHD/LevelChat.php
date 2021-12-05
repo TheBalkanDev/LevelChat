98d14ec46ac7d4b54aeceb0512592c12e3fbbe16
Baducai <65193314+Baducai@users.noreply.github.com>
2021-12-05 17:50:44 +0100

    Fixed for PM 4.0.0

--- src/iJoshuaHD/LevelChat.php
+++ src/iJoshuaHD/LevelChat.php
@@ -4,38 +4,43 @@ namespace iJoshuaHD;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
 
-class LevelChat extends PluginBase implements Listener{
+class LevelChat extends PluginBase implements Listener
+{
 
-	public function onLoad() : void{
-	$this->getLogger()->info(TextFormat::WHITE . "LevelChat - activated!");
-
-	public function onEnable(){
+    public function onLoad(): void
+    {
+        $this->getLogger()->info(TextFormat::WHITE . "LevelChat - activated!");
+    }
+    
+    public function onEnable(): void
+    {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-	public function onDisable(){
-		$this->getLogger()->info("[Level Chat] Disabled!");
+    public function onDisable(): void
+    {
+        $this->getLogger()->info("[Level Chat] Disabled!");
     }
 
-	public function onPlayerChat(PlayerChatEvent $ev){
-		$p = $ev->getPlayer();
-		$recipients = $ev->getRecipients();
-		$array = [];
-		foreach($recipients as $m => $t){
-			if($t instanceof Player){
-				if($p->getLevel() !== $t->getLevel()){
-					$array[] = $m;
-					foreach($array as $messages){
-						unset($recipients[$m]);
-						$ev->setRecipients(array_values($recipients));
-					}
-				}
-			}
-		}
-	}
-	
+    public function onPlayerChat(PlayerChatEvent $ev)
+    {
+        $p = $ev->getPlayer();
+        $recipients = $ev->getRecipients();
+        $array = [];
+        foreach ($recipients as $m => $t) {
+            if ($t instanceof Player) {
+                if ($p->getWorld() !== $t->getWorld()) {
+                    $array[] = $m;
+                    foreach ($array as $messages) {
+                        unset($recipients[$m]);
+                        $ev->setRecipients(array_values($recipients));
+                    }
+                }
+            }
+        }
+    }
+
 }
